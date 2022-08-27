6acf3346fd3f7d4c5b87b92504e8be0c398c9635
Tengiz Machavariani <tezimach@Tengizs-MacBook-Pro.local>
2022-08-27 21:28:43 +0400

    addedtemplate literal for comment replies in data.js, added classes for comment replies in style.css and added reply div in index.html.

--- data.js
+++ data.js
@@ -68,40 +68,67 @@ let data = {
       }
     ]
   }
-  
-console.log(data);
 
-console.log(data.comments);
 
 const commentsArray = data.comments;
 
 let commentHTML = "";
+let replyHTML = "";
 let commentSection = document.getElementById('comment-section');
 
+// comments
 for (let i=0; i<commentsArray.length; i++) {
-  let comment = commentsArray[i];
-  commentHTML += `<div id='${commentsArray[i].id}' class="comment">
-  <div id="header" class="header">
-    <img src=${commentsArray[i].user.image.png} alt="avatar" id="avatar" class="avatar">
-    <span class="font-header text-color-dark-blue">${commentsArray[i].user.username}</span>
-    <span class="font-paragraph">${commentsArray[i].createdAt}</span>
-  </div>
-  <p class="font-paragraph">${commentsArray[i].content}</p>
-  <div class="score font-header">
-    <input class="text-color-light-grey-blue" type="button" value="+">
-    <input class="text-color-moderate-blue" type="number" value='${commentsArray[i].score}' name="score" id="score">
-    <input class="text-color-light-grey-blue" type="button" value="-">
-  </div>
-  <button type="button">
-    <svg width="14" height="13" xmlns="http://www.w3.org/2000/svg"><path d="M.227 4.316 5.04.16a.657.657 0 0 1 1.085.497v2.189c4.392.05 7.875.93 7.875 5.093 0 1.68-1.082 3.344-2.279 4.214-.373.272-.905-.07-.767-.51 1.24-3.964-.588-5.017-4.829-5.078v2.404c0 .566-.664.86-1.085.496L.227 5.31a.657.657 0 0 1 0-.993Z" fill="#5357B6"/></svg>
-    <span class="font-header text-color-moderate-blue">Reply</span>
-  </button>
-</div>`
+    let comment = commentsArray[i];
+    commentHTML += `<div id='${comment.id}' class="comment">
+    <div id="header" class="header">
+      <img src=${comment.user.image.png} alt="avatar" id="avatar" class="avatar">
+      <span class="font-header text-color-dark-blue">${comment.user.username}</span>
+      <span class="font-paragraph">${comment.createdAt}</span>
+    </div>
+    <p class="font-paragraph">${comment.content}</p>
+    <div class="score font-header">
+      <input class="text-color-light-grey-blue" type="button" value="+">
+      <input class="text-color-moderate-blue" type="number" value='${comment.score}' name="score" id="score">
+      <input class="text-color-light-grey-blue" type="button" value="-">
+    </div>
+    <button type="button">
+      <svg width="14" height="13" xmlns="http://www.w3.org/2000/svg"><path d="M.227 4.316 5.04.16a.657.657 0 0 1 1.085.497v2.189c4.392.05 7.875.93 7.875 5.093 0 1.68-1.082 3.344-2.279 4.214-.373.272-.905-.07-.767-.51 1.24-3.964-.588-5.017-4.829-5.078v2.404c0 .566-.664.86-1.085.496L.227 5.31a.657.657 0 0 1 0-.993Z" fill="#5357B6"/></svg>
+      <span class="font-header text-color-moderate-blue">Reply</span>
+    </button>
+    </div>
+    <div id='reply ${comment.id}' class="reply">
+  </div>`
 
-commentSection.innerHTML = commentHTML;
+  commentSection.innerHTML = commentHTML;
+
+
+  //replies
+  let replies = document.getElementById(`reply ${comment.id}`);
+  const replyArray = comment.replies;
+
+  for (let i=0; i<replyArray.length; i++) {
+    let reply = replyArray[i];
+    replyHTML += `<aside></aside>
+    <div id='${reply.id}' class="comment">
+      <div id="header" class="header">
+        <img src=${reply.user.image.png} alt="avatar" id="avatar" class="avatar">
+        <span class="font-header text-color-dark-blue">${reply.user.username}</span>
+        <span class="font-paragraph">${reply.createdAt}</span>
+      </div>
+      <p class="font-paragraph">${reply.content}</p>
+      <div class="score font-header">
+        <input class="text-color-light-grey-blue" type="button" value="+">
+        <input class="text-color-moderate-blue" type="number" value='${reply.score}' name="score" id="score">
+        <input class="text-color-light-grey-blue" type="button" value="-">
+      </div>
+      <button type="button">
+        <svg width="14" height="13" xmlns="http://www.w3.org/2000/svg"><path d="M.227 4.316 5.04.16a.657.657 0 0 1 1.085.497v2.189c4.392.05 7.875.93 7.875 5.093 0 1.68-1.082 3.344-2.279 4.214-.373.272-.905-.07-.767-.51 1.24-3.964-.588-5.017-4.829-5.078v2.404c0 .566-.664.86-1.085.496L.227 5.31a.657.657 0 0 1 0-.993Z" fill="#5357B6"/></svg>
+        <span class="font-header text-color-moderate-blue">Reply</span>
+      </button>
+      </div>
+      <div id='reply ${reply.id}' class="reply">
+    </div>`
+
+    replies.innerHTML = replyHTML;
+  }
 }
-
-
-
-
-console.log(commentSection);
