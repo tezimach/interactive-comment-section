bf677148988815e1cb65d815ca9949250c90de97
Tengiz Machavariani <tezimach@Tengizs-MacBook-Pro.local>
2022-08-28 12:08:31 +0400

    modified code of data.js to be more readable, added form in index.html and it's classes and styles in style.css.

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-// import data from './data.json' assert {type: jason}
+//import data from './data.json' assert {type: jason}
 
 let data = {
     "currentUser": {
@@ -69,26 +69,48 @@ let data = {
     ]
   }
 
+// function that returns your comment's template literal
+  let commentTemplateYou = (el) => {
+      return`<div id='${el.id}' class="comment">
+      <div id="header" class="header">
+        <img src=${el.user.image.png} alt="avatar" id="avatar" class="avatar">
+        <span class="font-header text-color-dark-blue">${el.user.username}</span>
+        <span class="font-header text-white bg-blue you">you</span>
+        <span class="font-paragraph text-color-light-greysh-blue">${el.createdAt}</span>
+      </div>
+      <p class="font-paragraph text-color-light-greysh-blue">${el.content}</p>
+      <div class="score font-header">
+        <input class="text-color-light-grey-blue" type="button" value="+">
+        <input class="text-color-moderate-blue" type="number" value='${el.score}' name="score" id="score">
+        <input class="text-color-light-grey-blue" type="button" value="-">
+      </div>
+      <div class="buttons">
+        <button type="button" id="delete">
+          <svg width="12" height="14" xmlns="http://www.w3.org/2000/svg"><path d="M1.167 12.448c0 .854.7 1.552 1.555 1.552h6.222c.856 0 1.556-.698 1.556-1.552V3.5H1.167v8.948Zm10.5-11.281H8.75L7.773 0h-3.88l-.976 1.167H0v1.166h11.667V1.167Z" fill="#ED6368"/></svg>
+          <span class="font-header">Delete</span>
+        </button>
+        <button type="button" id="edit">
+          <svg width="14" height="14" xmlns="http://www.w3.org/2000/svg"><path d="M13.479 2.872 11.08.474a1.75 1.75 0 0 0-2.327-.06L.879 8.287a1.75 1.75 0 0 0-.5 1.06l-.375 3.648a.875.875 0 0 0 .875.954h.078l3.65-.333c.399-.04.773-.216 1.058-.499l7.875-7.875a1.68 1.68 0 0 0-.061-2.371Zm-2.975 2.923L8.159 3.449 9.865 1.7l2.389 2.39-1.75 1.706Z" fill="#5357B6"/></svg>
+          <span class="font-header text-color-moderate-blue">Edit</span>
+        </button>
+      </div>
+      </div>
+      <div id='reply ${el.id}' class="reply">
+    </div>`
+  }
 
-const commentsArray = data.comments;
-
-let commentHTML = "";
-let replyHTML = "";
-let commentSection = document.getElementById('comment-section');
-
-// comments
-for (let i=0; i<commentsArray.length; i++) {
-    let comment = commentsArray[i];
-    commentHTML += `<div id='${comment.id}' class="comment">
+  // function that returns other's comment's template literal
+  let commentTemplateOther = (el) => {
+    return `<div id='${el.id}' class="comment">
     <div id="header" class="header">
-      <img src=${comment.user.image.png} alt="avatar" id="avatar" class="avatar">
-      <span class="font-header text-color-dark-blue">${comment.user.username}</span>
-      <span class="font-paragraph">${comment.createdAt}</span>
+      <img src=${el.user.image.png} alt="avatar" id="avatar" class="avatar">
+      <span class="font-header text-color-dark-blue">${el.user.username}</span>
+      <span class="font-paragraph text-color-light-greysh-blue">${el.createdAt}</span>
     </div>
-    <p class="font-paragraph">${comment.content}</p>
+    <p class="font-paragraph text-color-light-greysh-blue">${el.content}</p>
     <div class="score font-header">
       <input class="text-color-light-grey-blue" type="button" value="+">
-      <input class="text-color-moderate-blue" type="number" value='${comment.score}' name="score" id="score">
+      <input class="text-color-moderate-blue" type="number" value='${el.score}' name="score" id="score">
       <input class="text-color-light-grey-blue" type="button" value="-">
     </div>
     <button type="button">
@@ -96,11 +118,27 @@ for (let i=0; i<commentsArray.length; i++) {
       <span class="font-header text-color-moderate-blue">Reply</span>
     </button>
     </div>
-    <div id='reply ${comment.id}' class="reply">
+    <div id='reply ${el.id}' class="reply">
   </div>`
+  }
 
+const commentsArray = data.comments;
+let commentHTML = "";
+let replyHTML = "";
+let commentSection = document.getElementById('comment-section');
+
+//function that runs through array and 
+//builds comments and replies based on it in html file
+//by template literal.
+const commentFunction = (array) => {
+  for (let i=0; i<array.length; i++) {
+    let comment = array[i];
+    if (comment.id == 4) {
+      commentHTML += commentTemplateYou(comment);
+    }else {
+      commentHTML += commentTemplateOther(comment);
   commentSection.innerHTML = commentHTML;
-
+    }
 
   //replies
   let replies = document.getElementById(`reply ${comment.id}`);
@@ -108,27 +146,18 @@ for (let i=0; i<commentsArray.length; i++) {
 
   for (let i=0; i<replyArray.length; i++) {
     let reply = replyArray[i];
-    replyHTML += `<aside></aside>
-    <div id='${reply.id}' class="comment">
-      <div id="header" class="header">
-        <img src=${reply.user.image.png} alt="avatar" id="avatar" class="avatar">
-        <span class="font-header text-color-dark-blue">${reply.user.username}</span>
-        <span class="font-paragraph">${reply.createdAt}</span>
-      </div>
-      <p class="font-paragraph">${reply.content}</p>
-      <div class="score font-header">
-        <input class="text-color-light-grey-blue" type="button" value="+">
-        <input class="text-color-moderate-blue" type="number" value='${reply.score}' name="score" id="score">
-        <input class="text-color-light-grey-blue" type="button" value="-">
-      </div>
-      <button type="button">
-        <svg width="14" height="13" xmlns="http://www.w3.org/2000/svg"><path d="M.227 4.316 5.04.16a.657.657 0 0 1 1.085.497v2.189c4.392.05 7.875.93 7.875 5.093 0 1.68-1.082 3.344-2.279 4.214-.373.272-.905-.07-.767-.51 1.24-3.964-.588-5.017-4.829-5.078v2.404c0 .566-.664.86-1.085.496L.227 5.31a.657.657 0 0 1 0-.993Z" fill="#5357B6"/></svg>
-        <span class="font-header text-color-moderate-blue">Reply</span>
-      </button>
-      </div>
-      <div id='reply ${reply.id}' class="reply">
-    </div>`
-
+    if (reply.id == 4) {
+      replyHTML += commentTemplateYou(reply);
+    }else{
+    replyHTML += commentTemplateOther(reply);
+    }
     replies.innerHTML = replyHTML;
+    }
   }
 }
+
+commentFunction(commentsArray);
+
+
+
+    
